940d3eceded1fc5e147ef8dd85435b52f00ae1ab
Nikhil Verma <nikhilverma9116@gmail.com>
2026-08-16 02:39:22 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,15 @@
+-- Create Database 'Data WareHouse'
+USE master;
+
+CREATE DATABASE DataWareHouse;
+
+USE DataWareHouse;
+
+-- Create Schema's
+
+CREATE SCHEMA bronze;
+Go
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
